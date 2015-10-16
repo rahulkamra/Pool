d530267e5c3d710679e1433b1042b55d2c37cdf2
rahulkamra <rahulkamra@gmail.com>
2015-10-16 09:45:17 +0400

    Setup balls physics
    Setup balls and changed some forces configuration

---
+++ .localhistory/Assets/src/1444972185$TestCue.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCue : MonoBehaviour
+{
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        this.GetComponent<Rigidbody>()
+    }
+}
---
+++ .localhistory/Assets/src/1444972245$TestCue.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCue : MonoBehaviour
+{
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Rigidbody rigidbody = this.GetComponent<Rigidbody>();
+        rigidbody.AddForce(Vector3(0,0,10));
+    }
+}
